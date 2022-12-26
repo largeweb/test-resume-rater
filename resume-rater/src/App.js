ddacb344b1208d093767568b58cff183afb4bdf8
matt <mat@unc.edu>
2022-12-25 22:13:01 -0500

    test

--- resume-rater/src/App.js
+++ resume-rater/src/App.js
@@ -1,10 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import React,{useState} from 'react';
+import {Document, Page, pdfjs } from 'react-pdf';
+
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 function App() {
+
+  const [samplePDF, setSamplePDF] = useState(null);
+  const [numPages, setNumPages] = useState(null);
+  const [pageNumber, setPageNumber] = useState(1);
+
+  function onDocumentLoadSuccess({ numPages }) {
+    setNumPages(1);
+  }
+
+  const handleFileChange = (e) => {
+    if (e.target.files) {
+      setSamplePDF(e.target.files[0]);
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <ReactPDF file={{data:renderPDF}}></ReactPDF> */}
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +37,18 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      <div>Test</div>
+      <input type="file" onChange={handleFileChange} />
+      {/* <Document file={samplePDF}>
+        <Page pageNumber={1} />
+      </Document> */}
+      <Document file={samplePDF} onLoadSuccess={onDocumentLoadSuccess}>
+        <Page pageNumber={pageNumber} />
+      </Document>
+      <p>
+        Page {pageNumber} of {numPages}
+      </p>
     </div>
   );
 }
